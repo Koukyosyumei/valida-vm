7c4f7b3cda2063b9bca0049090f573c431d32667
Koukyosyumei <koukyosyumei@hotmail.com>
2025-09-30 23:09:14 -0400

    malformed is_last_segment

--- basic-api/tests/test_prover.rs
+++ basic-api/tests/test_prover.rs
@@ -1076,7 +1076,7 @@ fn expected_sdiv_memory_state(memory_backend: &ValidaMemoryBackend) {
 fn prove_small_add() {
     let program = add_program::<BabyBear>();
     let (_machine, _memory_backend) = prove_program(program, ProgramTableType::Public);
-    assert!(false, "Verification should fail.");
+    assert!(false, "Unexpected Success: Verification should fail.");
 }
 
 #[test]
